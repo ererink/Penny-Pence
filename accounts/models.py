7cc3f726039a7f19026f7bb9308e8de6a89e9e46
ererink <yelkim0210@gmail.com>
2023-02-05 21:01:36 +0900

    feat: 카카오 로그인 urls.py작성 #6
    - 카카오 로그인 urls.py 작성
    - 카카오 로그인 views.py 토큰 요청 작성 중

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from back.settings import AUTH_USER_MODEL
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+   nickname = models.CharField(max_length=10, blank=True)
+   refresh_token = models.TextField(blank=True)
+   profile_img = models.TextField(blank=True) 
+   
