83664a098230950cae4e3c203cc9de9ec08764e7
tnpfldyd <dkdldhs0322@gmail.com>
2023-02-15 07:28:51 +0900

    refact: 코드 리팩토링

--- questions/models.py
+++ questions/models.py
@@ -1,42 +1,40 @@
-# from django.contrib.contenttypes.models import ContentType
-# from django.contrib.contenttypes.fields import GenericForeignKey
-from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
-
+from django.conf import settings
 
 class Question(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    like_count = models.PositiveIntegerField(default=0)
 
+    class Meta:
+        ordering = ['-created_at']
+        
     def __str__(self):
         return self.title
 
-
 class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='comments')
-    content = models.CharField(max_length=200)
-    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    like_count = models.PositiveIntegerField(default=0)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
-        return f'Comment by {self.user} on {self.question}'
-
+        return self.content
 
 class Like(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content_type = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = models.GenericForeignKey('content_type', 'object_id')
-
-    class Meta:
-        unique_together = ('user', 'content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
-        return f'{self.user} likes {self.content_object}'
+        return f"{self.content_object} liked by {self.user.username}"
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from .models import Question, Comment, Like
 from .serializers import QuestionSerializer, CommentSerializer, LikeSerializer
 
@@ -8,20 +9,37 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        data['like_count'] = instance.likes.count()
+        return Response(data)
+
     @action(detail=True, methods=['get'])
     def comments(self, request, pk=None):
         question = self.get_object()
-        comments = Comment.objects.filter(question=question)
+        comments = Comment.objects.filter(question=question, parent=None).prefetch_related('likes', 'replies__likes')
         serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
+        data = serializer.data
+        return Response(data)
 
     @action(detail=True, methods=['post'])
     def like(self, request, pk=None):
         question = self.get_object()
         user = request.user
-        like, created = Like.objects.get_or_create(user=user, question=question)
-        serializer = LikeSerializer(like)
-        return Response(serializer.data)
+        try:
+            like = Like.objects.get(question=question, user=user)
+            like.delete()
+            return Response({'detail': 'Question unliked.'})
+        except Like.DoesNotExist:
+            like = Like(question=question, user=user)
+            like.save()
+            return Response({'detail': 'Question liked.'})
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
@@ -30,9 +48,44 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class LikeViewSet(viewsets.ModelViewSet):
-    queryset = Like.objects.all()
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        comment = self.get_object()
+        user = request.user
+        try:
+            like = Like.objects.get(comment=comment, user=user)
+            like.delete()
+            return Response({'detail': 'Comment unliked.'})
+        except Like.DoesNotExist:
+            like = Like(comment=comment, user=user)
+            like.save()
+            return Response({'detail': 'Comment liked.'})
+        
+    @action(detail=True, methods=['post'])
+    def replies(self, request, pk=None):
+        parent_comment = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=self.request.user, parent=parent_comment)
+        return Response(serializer.data)
+
+class LikeViewSet(viewsets.ViewSet):
     serializer_class = LikeSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    @action(detail=False, methods=['get'])
+    def question_likes(self, request): 
+        question_id = request.query_params.get('question_id', None)
+        if not question_id:
+            raise ValidationError("Question ID must be provided as a query parameter.")
+        likes = Like.objects.filter(question_id=question_id)
+        serializer = self.serializer_class(likes, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def comment_likes(self, request):
+        comment_id = request.query_params.get('comment_id', None)
+        if not comment_id:
+            raise ValidationError("Comment ID must be provided as a query parameter.")
+        likes = Like.objects.filter(comment_id=comment_id)
+        serializer = self.serializer_class(likes, many=True)
+        return Response(serializer.data)
