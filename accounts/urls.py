e8033478efe452e2eca696e60f5333183e9a4a87
ererink <yelkim0210@gmail.com>
2023-02-14 20:57:06 +0900

    feat: 카카오 로그인 구현 중 #9
    - accounts/settings, urls, views 작성 중

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path
-from . import views
 from django import views
-
-app_name = "accounts"
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('registration/', include('dj_rest_auth.registration.urls')),
     # 카카오 소셜 로그인
-    path("login/kakao", views.kakao_login, name="kakao_login"),
-    path("kakao/callback", views.kakao_callback, name="kakao_callback"),
+    path('kakao/login/', views.kakao_login, name='kakao_login'),
+    path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
     path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render, redirect
-import os
+import env
 import requests
 from json.decoder import JSONDecodeError
 from accounts.models import User
+from allauth.socialaccount.models import SocialAccount
+from django.http import JsonResponse
+from rest_framework import status
+from json.decoder import JSONDecodeError
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.kakao import views as kakao_view
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 # Create your views here.
-BASE_URL = 'https://pennypence-backend.shop/'
+BASE_URL = 'http://127.0.0.1:8000/'
 KAKAO_CALLBACK_URI = BASE_URL + 'accounts/kakao/callback/'
 
 
 # 카카오 로그인
 def kakao_login(request):
-    rest_api_key = os.getenv("KAKAO_REST_API_KEY")
+    rest_api_key = env.KAKAO_REST_API_KEY
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={rest_api_key}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code"
     )
 
 def kakao_callback(request):
-    rest_api_key = os.getenv("KAKAO_REST_API_KEY")
+    rest_api_key = env.KAKAO_REST_API_KEY
     code = request.GET.get("code")
     redirect_uri = "https://master.d3n2xysrd0lvj9.amplifyapp.com/oauth/callback/kakao"
     
@@ -42,4 +49,31 @@ def kakao_callback(request):
     
     email = kakao_account.get('email')
 
-    user = User.objects.get(email=email)
+    # Signup or Login Request
+    try:
+        user = User.objects.get(email=email)                # 이메일로 로그인
+        social_user = SocialAccount.objects.get(user=user)
+        
+        if social_user is None:
+            return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # 가입된 유저가 아닐 시 가입
+    except User.DoesNotExist:
+        data = {'access_token': access_token, 'code': code}
+        accept = requests.post(
+            f"{BASE_URL}accounts/kakao/login/finish/", data=data
+            )
+        accept_status = accept.status_code
+        
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
+        
+        # 사용자의 pk, email, first name, last name과 Access Token, Refresh token 가져오기
+        accept_json = accept.json()
+        print(accept_json)  
+        return JsonResponse(accept_json)
+
+class KakaoLogin(SocialLoginView):
+    adapter_class = kakao_view.KakaoOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = KAKAO_CALLBACK_URI
