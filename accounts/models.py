c612c5da96cf18acfe2c6a8d70e8ee3f731737a8
ererink <yelkim0210@gmail.com>
2023-02-14 23:58:47 +0900

    feat: 카카오 소셜 로그인 구현 #12
    - 카카오 소셜 로그인 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from back.settings import AUTH_USER_MODEL
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class User(AbstractUser):
-   nickname = models.CharField(max_length=10, blank=True)
+   email = models.EmailField(_('email address'), unique=True)
+   nickname = models.CharField(max_length=8, blank=True)
    refresh_token = models.TextField(blank=True)
    profile_img = models.TextField(blank=True) 
-   
+
+   USERNAME_FIELD = 'email'
+   REQUIRED_FIELDS = []
