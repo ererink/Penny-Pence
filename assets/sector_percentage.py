a62c2513d0a4d824b69c4e4302bad001c91a5644
Myeonghak Lee <mhmh779@naver.com>
2023-02-17 23:18:42 +0900

    News리스트 데이터저장 코드 추가

--- assets/sector_percentage.py
+++ assets/sector_percentage.py
@@ -19,25 +19,22 @@ def it_sector():
             for row in reader:
                 column_data.append(row[column_index])
             new_column_data = deque(list(filter(None, column_data))) # 빈 열 제거
-            # print(new_column_data)
             new_column_data.pop() # 마지막 요소 제거
-            # print(len(new_column_data))
-            # print(new_column_data)
-            # print(len(new_column_data))
+
             for i in range(len(new_column_data)):
                 number = new_column_data.popleft()
                 it_average[i] += float(number)
-                # print(number)
-
+  
     for i in it_average:
         num = round(i/3, 2)
         it_AVG.append(num)
+    print(it_AVG)
     return it_AVG
 
 # Game 섹터
 def game_sector():
     game_average = [0] * 40
-    game_AVG = []
+    game_AVG = deque()
     game_address = deque(["C:\\Users\\이명학\\Desktop\\주가 10년\\게임\\엔씨소프트 주가(10년).csv",
                           "C:\\Users\\이명학\\Desktop\\주가 10년\\게임\\위메이드 주가(10년).csv", 
                           "C:\\Users\\이명학\\Desktop\\주가 10년\\게임\\컴투스 주가(10년).csv"])
@@ -51,27 +48,24 @@ def game_sector():
             for row in reader:
                 column_data.append(row[column_index])
             new_column_data = deque(list(filter(None, column_data))) # 빈 열 제거
-            # print(new_column_data)
             new_column_data.pop() # 마지막 요소 제거
-            # print(len(new_column_data))
-            # print(new_column_data)
-            # print(len(new_column_data))
+
             for i in range(len(new_column_data)):
                 number = new_column_data.popleft()
                 game_average[i] += float(number)
-                # print(number)
 
     for i in game_average:
         num = round(i/3, 2)
         game_AVG.append(num)
     print(game_AVG)
+    return game_AVG
 
 
 
 # Bio 섹터
 def bio_sector():
     bio_average = [0] * 40
-    bio_AVG = []
+    bio_AVG = deque()
     bio_address = deque(["C:\\Users\\이명학\\Desktop\\주가 10년\\바이오\\셀트리온 주가(10년).csv", 
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\바이오\\신풍제약 주가(10년).csv",
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\바이오\\유한양행 주가(10년).csv"])
@@ -85,27 +79,24 @@ def bio_sector():
             for row in reader:
                 column_data.append(row[column_index])
             new_column_data = deque(list(filter(None, column_data))) # 빈 열 제거
-            # print(new_column_data)
             new_column_data.pop() # 마지막 요소 제거
-            # print(len(new_column_data))
-            # print(new_column_data)
-            # print(len(new_column_data))
+
             for i in range(len(new_column_data)):
                 number = new_column_data.popleft()
                 bio_average[i] += float(number)
-                # print(number)
 
     for i in bio_average:
         num = round(i/3, 2)
         bio_AVG.append(num)
     print(bio_AVG)
+    return bio_AVG
 
 
 
 # 반도체 섹터
 def sc_sector():
     sc_average = [0] * 40
-    sc_AVG = []
+    sc_AVG = deque()
     sc_address = deque(["C:\\Users\\이명학\\Desktop\\주가 10년\\반도체\\LG디스플레이 주가(10년).csv", 
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\반도체\\삼성전자 주가(10년).csv",
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\반도체\\삼성전자 주가(10년).csv"])
@@ -119,26 +110,23 @@ def sc_sector():
             for row in reader:
                 column_data.append(row[column_index])
             new_column_data = deque(list(filter(None, column_data))) # 빈 열 제거
-            # print(new_column_data)
             new_column_data.pop() # 마지막 요소 제거
-            # print(len(new_column_data))
-            # print(new_column_data)
-            # print(len(new_column_data))
+
             for i in range(len(new_column_data)):
                 number = new_column_data.popleft()
                 sc_average[i] += float(number)
-                # print(number)
 
     for i in sc_average:
         num = round(i/3, 2)
         sc_AVG.append(num)
     print(sc_AVG)
+    return sc_AVG
 
 
 # 엔터테인먼트 섹터
 def enter_sector():
     enter_average = [0] * 40
-    enter_AVG = []
+    enter_AVG = deque()
     enter_address = deque(["C:\\Users\\이명학\\Desktop\\주가 10년\\엔터\\JPY 주가(10년).csv", 
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\엔터\\SM 주가(10년).csv",
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\엔터\\YG플러스 주가(10년).csv"])
@@ -152,26 +140,23 @@ def enter_sector():
             for row in reader:
                 column_data.append(row[column_index])
             new_column_data = deque(list(filter(None, column_data))) # 빈 열 제거
-            # print(new_column_data)
             new_column_data.pop() # 마지막 요소 제거
-            # print(len(new_column_data))
-            # print(new_column_data)
-            # print(len(new_column_data))
+
             for i in range(len(new_column_data)):
                 number = new_column_data.popleft()
                 enter_average[i] += float(number)
-                # print(number)
 
     for i in enter_average:
         num = round(i/3, 2)
         enter_AVG.append(num)
     print(enter_AVG)
+    return enter_AVG
 
 
 # 자동차 섹터
-def automobile_sec():
+def automobile_sector():
     automobile_average = [0] * 40
-    automobile_AVG = []
+    automobile_AVG = deque()
     automobile_address = deque(["C:\\Users\\이명학\\Desktop\\주가 10년\\자동차\\기아차 주가(10년).csv", 
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\자동차\\현대모비스 주가(10년).csv",
                         "C:\\Users\\이명학\\Desktop\\주가 10년\\자동차\\현대차 주가(10년).csv"])
@@ -185,26 +170,23 @@ def automobile_sec():
             for row in reader:
                 column_data.append(row[column_index])
             new_column_data = deque(list(filter(None, column_data))) # 빈 열 제거
-            # print(new_column_data)
             new_column_data.pop() # 마지막 요소 제거
-            # print(len(new_column_data))
-            # print(new_column_data)
-            # print(len(new_column_data))
+
             for i in range(len(new_column_data)):
                 number = new_column_data.popleft()
                 automobile_average[i] += float(number)
-                # print(number)
 
     for i in automobile_average:
         num = round(i/3, 2)
         automobile_AVG.append(num)
     print(automobile_AVG)
+    return automobile_AVG
 
 
-# if __name__ == "__main__":
-it_sector()
-# game_sector()
-# bio_sector()
-# sc_sector()
-# enter_sector()
-# automobile_sec()
+if __name__ == "__main__":
+    it_sector()
+    game_sector()
+    bio_sector()
+    sc_sector()
+    enter_sector()
+    automobile_sector()
--- assets/urls.py
+++ assets/urls.py
@@ -3,23 +3,18 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import RankingViewSet, NewsViewSet
 from . import views
 
-# # news 리스트 보여주기
-# news_list = NewsViewSet.as_view({
-#     'get': 'list',
-#     # 'post': 'create'
-# })
 
-# ranking 리스트 보여주기
 ranking_list = RankingViewSet.as_view({
     'get': 'list'
 })
 
-
 urlpatterns = [
-    # path('news/', news_list),
+    path('news/', views.create_news),
     path('ranking/', ranking_list),
-    path('game/', views.date_create),
     path('sector/', views.sector),
+
+    # DB 저장용[사용안하는 주소]
+    # path('game/', views.date_create),
     # path('data_input/', views.save_sector)
 ]
 
