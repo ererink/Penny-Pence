62ce5d5de7c5cacb470e687432cbba03987ae307
ererink <yelkim0210@gmail.com>
2023-02-19 15:13:38 +0900

    fix: managers.py 작성
    username 오류를 위한 managers.py 작성

---
+++ accounts/managers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
