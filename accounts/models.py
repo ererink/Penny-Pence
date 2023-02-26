3bf21d261e0e5d2fb252ad5bf228346bd3bc0aa9
ererink <yelkim0210@gmail.com>
2023-02-26 16:20:01 +0900

    Merge branch 'develop' of https://github.com/PennyPence/pennypence-drf-backend into YL-14

--- accounts/models.py
+++ accounts/models.py
@@ -4,40 +4,41 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from items.models import Item
 from .managers import CustomUserManager
-from imagekit.models import ImageSpecField
+from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
 # Create your models here.
 
 class User(AbstractUser):
-   # 기본정보
-   email = models.EmailField(_('email address'), unique=True)
-   nickname = models.CharField(max_length=8, blank=True)
-   refresh_token = models.TextField(blank=True)
-   profile_img = models.TextField(blank=True)                           # 카카오에서 받은 프로필 이미지 (URL) 
-   edited_img = ImageSpecField(source='profile_img',
-                                processors=[ResizeToFill(1200, 960)],
-                                format='JPEG',
-                                options={'quality': 80})                 # 수정한 프로필 이미지 (JPEG) 
-   username = None
-   
-   # 부가정보
-   money = models.IntegerField(default=100000, blank=True)                       # 자산 (임시로 blank=True 해놓음)
-   school = models.CharField(max_length=10, blank=True)
-   # sector = models.ForeignKey(Sectors, on_delete=models.CASCADE)
-      # 매수목록
-   # position = models.ManyToManyField(Sectors, through=User_Positions)
-   # 아이템 
-   inventory = models.ManyToManyField(Item, through='User_Items')
-   
-   # 기능
-   followers = models.ManyToManyField('self', symmetrical=True, related_name='following')    # Ture: 양방향 관계, 일촌 개념
+    # 기본정보
+    email = models.EmailField(_('email address'), unique=True)
+    nickname = models.CharField(max_length=8, blank=True)
+    refresh_token = models.TextField(blank=True)
+    profile_img = models.TextField(blank=True)                           # 카카오에서 받은 프로필 이미지 (URL) 
+    edited_img = ProcessedImageField(upload_to='images/', blank=True,
+                                 processors=[ResizeToFill(1200, 960)],
+                                 format='JPEG',
+                                 options={'quality': 80},
+                                 null=True)                              # 수정한 프로필 이미지 (JPEG) 
+    username = None
 
-   
-   objects = CustomUserManager()
-   
-   USERNAME_FIELD = 'email'
-   REQUIRED_FIELDS = []
+    # 부가정보
+    money = models.IntegerField(default=100000, blank=True)                       # 자산 (임시로 blank=True 해놓음)
+    school = models.CharField(max_length=10, blank=True)
+    # sector = models.ForeignKey(Sectors, on_delete=models.CASCADE)
+      # 매수목록
+    # position = models.ManyToManyField(Sectors, through=User_Positions)
+    # 아이템 
+    inventory = models.ManyToManyField(Item, through='User_Items')
+
+    # 기능
+    followers = models.ManyToManyField('self', symmetrical=True, related_name='following')    # Ture: 양방향 관계, 일촌 개념
+ 
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
    # 매수목록 ManyToManyField 확장
 # class User_Positions(models.Model):
@@ -49,5 +50,5 @@ class User(AbstractUser):
 
 # 아이템목록(inventory) ManyToManyField 확장
 class User_Items(models.Model):
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
