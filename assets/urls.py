bad8e992a3a02955f573bea18e0cd38dc2c2b8bc
Myeonghak Lee <mhmh779@naver.com>
2023-03-11 12:51:02 +0900

    CICD

--- assets/urls.py
+++ assets/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('', include(router.urls)),
     # path('ranking/<int:pk>/', RankingViewSet.as_view({'get': 'get_ranking_by_game_date'}), name='ranking-game-date')
     path('v1/test/', TestView.as_view(), name='test'),
-
+    
     # DB 저장용[사용안하는 주소]
     # path('game/', views.date_create),
     # path('data_input/', views.save_sector)
